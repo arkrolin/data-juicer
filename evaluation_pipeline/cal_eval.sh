a3ab41df034321b3e9c35ff4c9e57542d7e75022
Qirui-jiao <luckybanana404@gmail.com>
2026-05-15 16:52:42 +0800

    Update README.

--- evaluation_pipeline/cal_eval.sh
+++
@@ -1,3 +0,0 @@
-python evaluation_pipeline/cal_eval.py \
---eval_output_log_dir_name ./playground/evaluation \
---name_prefix your_model_s_name
--- evaluation_pipeline/eval_mini_benchmark/cal_eval_mini.sh
+++
@@ -1,3 +0,0 @@
-python evaluation_pipeline/eval_mini_benchmark/cal_eval_mini.py \
---eval_output_log_dir_name ./playground/evaluation \
---name_prefix your_model_s_name
